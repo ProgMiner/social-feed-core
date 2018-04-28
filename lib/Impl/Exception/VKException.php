3e98e9f6d85dabd1245c964a3a2827226c234fe7
ProgMiner <eridan200@mail.ru>
2018-04-28 14:05:18 +0300

    Added APIException, VKPostProvider and VKException

---
+++ lib/Impl/Exception/VKException.php
@@ -0,0 +1,65 @@
+<?php
+
+/* MIT License
+
+Copyright (c) 2018 Eridan Domoratskiy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+namespace SocialFeedCore\Impl\Exception;
+
+use SocialFeedCore\Exception\APIException;
+
+/**
+ * VK API exception
+ *
+ * @author ProgMiner
+ */
+class VKException extends APIException {
+
+    /**
+     * @param mixed      $request  Request that caused an error
+     * @param \stdClass  $response Full response from API with error
+     * @param \Throwable $previous Previous exception
+     */
+    public function __construct($request, \stdClass $response, \Throwable $previous = null) {
+        parent::__construct(
+            $request,
+            $response,
+            $response->error->error_msg,
+            $response->error->error_code,
+            $previous
+        );
+    }
+
+    /**
+     * Checks for an error and throws an exception
+     * if it contains an error
+     *
+     * @param mixed     $request  Request that caused an error
+     * @param \stdClass $response Full response from API with error
+     *
+     * @throws static
+     */
+    public static function checkError($request, \stdClass $response) {
+        if (isset($response->error)) {
+            throw new static($request, $response);
+        }
+    }
+}
---
+++ lib/Impl/VKPostProvider.php
@@ -0,0 +1,70 @@
+<?php
+
+/* MIT License
+
+Copyright (c) 2018 Eridan Domoratskiy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+namespace SocialFeedCore\Impl;
+
+use SocialFeedCore\IPostProvider;
+
+use SocialFeedCore\Utility\RequestOptions;
+
+use SocialFeedCore\Impl\Exception\VKException;
+
+/**
+ * VK.com post provider
+ *
+ * @author ProgMiner
+ */
+class VKPostProvider implements IPostProvider {
+
+    /**
+     * Requests VK API method
+     *
+     * @param string   $method     API method name
+     * @param string[] $params     Request parameters
+     * @param bool     $checkError If true checks is result contains error 
+     *                             and throw exception if contains
+     *
+     * @return mixed Response object if $checkError = true, full result object otherwise
+     *
+     * @throws VKException
+     */
+    public static function api(string $method, array $params = [], bool $checkError = true) {
+        $request = "https://api.vk.com/method/{$method}?".http_build_query($params);
+        $result = json_decode(file_get_contents($request));
+
+        if (!$checkError) {
+            return $result;
+        }
+
+        VKException::checkError($request, $result);
+        return $result;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPosts(RequestOptions $options): array {
+        //
+    }
+}
